1442bd78624ac894135b89967932ea25c587634a
ErwanLT <erwanletutour.elt@gmail.com>
2017-03-30 07:19:33 +0200

    Delete SportEquipe.java

--- src/abstractClasse/SportEquipe.java
+++
@@ -1,7 +0,0 @@
-package abstractClasse;
-
-public abstract class SportEquipe implements SportInterface {
-
-	protected abstract String equipeAvecPlusHautScore(int scoreJoueur1, int scoreJoueur2);
-
-}
